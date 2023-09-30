c5e7d02ecc172e196cfe7ac1fb181dbcf526f852
hinpa <masterz4pl@gmail.com>
2023-09-30 20:31:32 +0300

    add function templates

---
+++ src/func/func.cpp
@@ -0,0 +1,29 @@
+
+
+#include "func.h"
+#include <cmath>
+#include <cstdarg>
+#include <initializer_list>
+#include "../exception/except.h"
+#include "../token/token.h"
+
+
+double p_sin(Node<Token> t)      { throw NotImplemented("This math function is not implemented");}
+double p_cos(Node<Token> t)      { throw NotImplemented("This math function is not implemented"); }
+double p_tan(Node<Token> t)      { throw NotImplemented("This math function is not implemented"); }
+double p_cotan(Node<Token> t)    { throw NotImplemented("This math function is not implemented"); }
+double p_asin(Node<Token> t)     { throw NotImplemented("This math function is not implemented"); }
+double p_acos(Node<Token> t)     { throw NotImplemented("This math function is not implemented"); }
+double p_atan(Node<Token> t)     { throw NotImplemented("This math function is not implemented"); }
+double p_acotan(Node<Token> t)   { throw NotImplemented("This math function is not implemented"); }
+double p_mod(Node<Token> t)      { throw NotImplemented("This math function is not implemented"); }
+double p_ln(Node<Token> t)       { throw NotImplemented("This math function is not implemented"); }
+double p_log(Node<Token> t)      { throw NotImplemented("This math function is not implemented"); }
+double p_plus(Node<Token> t)     { throw NotImplemented("This math function is not implemented"); }
+double p_minus(Node<Token> t)    { throw NotImplemented("This math function is not implemented"); }
+double p_mult(Node<Token> t)     { throw NotImplemented("This math function is not implemented"); }
+double p_pow(Node<Token> t)      { throw NotImplemented("This math function is not implemented"); }
+double p_div(Node<Token> t)      { throw NotImplemented("This math function is not implemented"); }
+double p_toNumber(Node<Token> t) { throw NotImplemented("This math function is not implemented"); }
+double p_list(Node<Token> t)     { throw NotImplemented("This math function is not implemented"); }
+
---
+++ src/func/func.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <initializer_list>
+#include "../tree/node.h"
+#include "../token/token.h"
+
+double p_toNumber(Node<Token> t);
+double p_list(Node<Token> t); // traverses all adjacent ',' operators
+double p_sin(Node<Token> t);
+double p_cos(Node<Token> t);
+double p_tan(Node<Token> t);
+double p_cotan(Node<Token> t);
+double p_asin(Node<Token> t);
+double p_acos(Node<Token> t);
+double p_atan(Node<Token> t);
+double p_acotan(Node<Token> t);
+double p_mod(Node<Token> t);
+double p_ln(Node<Token> t);
+double p_log(Node<Token> t);
+double p_plus(Node<Token> t);
+double p_minus(Node<Token> t);
+double p_mult(Node<Token> t);
+double p_pow(Node<Token> t);
+double p_div(Node<Token> t);
